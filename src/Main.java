2e21fffb803b4c2bbe426130615fd117065e0c1a
Aleksandr <a-l-e-x-007@yandex.ru>
2020-06-03 11:05:06 +0300

    update srs

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,11 @@ public class Main {
         System.out.println("hello!");
         System.out.println(555);
     }
+
+    static class Main2 {
+        public  static void  main(String[] args){
+            System.out.println("hello2!");
+            System.out.println(222);
+        }
+    }
 }
