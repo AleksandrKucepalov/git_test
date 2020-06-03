6f4edd79fc8c64cf8bd6b1bed770cf5eeed8d967
AleksandrKucepalov <66354243+AleksandrKucepalov@users.noreply.github.com>
2020-06-03 11:02:41 +0300

    Update Maim2.java

--- src/Maim2.java
+++ src/Maim2.java
@@ -1,6 +1,6 @@
-public class Main2 {
+class Main2 {
     public  static void  main(String[] args){
         System.out.println("hello2!");
-        System.out.println(222);
+        System.out.println(2222);
     }
 }
